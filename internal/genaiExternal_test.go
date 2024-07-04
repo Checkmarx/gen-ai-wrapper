de745dfc107075bbbdcf4e47ae1a4b377f316570
BarakH <barak.harari@checkmarx.com>
2024-07-04 09:13:27 +0300

    unit test error handling, Azure,OpenAi

--- internal/genaiExternal_test.go
+++ internal/genaiExternal_test.go
@@ -7,22 +7,17 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"testing"
 )
 
 func TestHandleGptResponse(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.WriteHeader(http.StatusOK)
-		_, _ = res.Write([]byte("test"))
-	}))
-	defer testServer.Close()
 	wrapper := WrapperImpl{
 		apiKey:   "test",
-		endPoint: testServer.URL,
+		endPoint: "testServer.URL",
 		dropLen:  4}
-	resp := &http.Response{}
+	resp := &http.Response{Body: io.NopCloser(bytes.NewReader([]byte(RespBody)))}
+	resp.StatusCode = http.StatusOK
 	_, err := wrapper.handleGptResponse("test", &message.MetaData{}, &ChatCompletionRequest{}, resp)
 	if err != nil {
 		t.Fatal(err)
@@ -128,3 +123,15 @@ func TestHandleGptResponseNegativeAzureInternal(t *testing.T) {
 	assert.Contains(t, err.Error(), strconv.Itoa(0))
 	assert.Contains(t, err.Error(), strconv.Itoa(http.StatusInternalServerError))
 }
+
+//nolint:lll
+const (
+	RespBody = `{
+"id":"chatcmpl-8v26ZSbJu0DEIbeyLVG3NT8m7imGl",
+"object":"chat.completion","created":1708603479,
+"model":"gpt-4","choices":[{
+"finish_reason":"stop","index":0,"message":
+{"role":"assistant","content":"The impact of the 'ALB Deletion Protection Disabled' issue is potentially significant, depending on the criticality of the AWS Application Load Balancer (ALB) to your applications or services. Since 'enable_deletion_protection' is currently set to 'false', it means deletion protection is not enabled on your ALB. This allows anyone with the appropriate permissions to delete your ALB. If this load balancer is deleted, either accidentally or maliciously, it can lead to service disruptions as the traffic meant for your applications will not be distributed. Particularly in a production environment, such a service disruption can lead to loss of availability or potentially even loss of business or revenue, especially if the recovery and restoration time is lengthy. This is why it's recommended to set the 'enable_deletion_protection' field to 'true', especially when dealing with ALBs in production environments. "}}],
+"usage":{"prompt_tokens":849,"completion_tokens":181,"total_tokens":1030}}
+`
+)
